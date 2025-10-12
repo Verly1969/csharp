336b68c648fffd651787dd949d7881d0b985d30a
Eddy Verly <eddyverly@icloud.com>
2025-10-12 10:16:39 +0200

    mystery app

---
+++ NombreMystere/NombreMystere/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+ Find the mystery number
+ */
+using System;
+
+int life = 5;
+int min = 0;
+int max = 100;
+int mystery = 0;
+
+Random random = new Random();
+mystery = random.Next(min, max);
+
+Console.WriteLine(mystery.ToString());
